9e7e1a4fdedc57a2f21e96e5eb553da8a0cf1e1f
Merkie <merkie@post.com>
2023-08-01 11:57:21 -0500

    Make header more responsive

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -1,13 +1,26 @@
-<div class="fixed left-0 top-0 flex w-screen items-center gap-8 p-2 font-mono">
-	<a href="/" class="text-stone-500 hover:text-orange-500">tailwind-typer</a>
-	<a
-		href="https://github.com/merkie/tailwind-typer"
-		target="_blank"
-		class="text-xs text-stone-500 hover:text-orange-500"
-	>
-		<i class="bi bi-github" /> Fork this project
-	</a>
+<div class="fixed left-0 top-0 flex w-screen items-start gap-8 p-2 font-mono md:items-center">
+	<div class="flex items-center gap-8">
+		<a href="/" class="text-stone-500 hover:text-orange-500">tailwind-typer</a>
+		<a
+			href="https://github.com/merkie/tailwind-typer"
+			target="_blank"
+			class="hidden items-center gap-2 text-xs text-stone-500 hover:text-orange-500 md:flex"
+		>
+			<i class="bi bi-github text-xl md:text-xs" />
+			Fork this project
+		</a>
+	</div>
 	<div class="flex-1" />
-	<a href="/about" class="text-xs text-stone-500 hover:text-orange-500">What is this?</a>
-	<a href="/components" class="text-xs text-stone-500 hover:text-orange-500">Components</a>
+	<div class="flex flex-col items-end gap-2 md:flex-row md:items-center md:gap-8">
+		<a href="/about" class="text-xs text-stone-500 hover:text-orange-500">What is this?</a>
+		<a href="/components" class="text-xs text-stone-500 hover:text-orange-500">Components</a>
+		<a
+			href="https://github.com/merkie/tailwind-typer"
+			target="_blank"
+			class="flex items-center gap-2 text-xs text-stone-500 hover:text-orange-500 md:hidden"
+		>
+			<i class="bi bi-github" />
+			Fork this project
+		</a>
+	</div>
 </div>
